dc3158147b1d7a9906302aed3cd1f7c38fb6a999
小C <616345713@qq.com>
2022-05-14 12:33:10 +0800

    test code

---
+++ src/main/java/study/xiachao/mvn/ClassMvnTest.java
@@ -0,0 +1,29 @@
+package study.xiachao.mvn;
+
+/**
+ * ClassMvnTest
+ *
+ * @author xiachao
+ * @date 2022/5/14
+ */
+public class ClassMvnTest {
+
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
